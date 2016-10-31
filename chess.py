76dcad4c9738b28ada293beccd814645160464f7
Nicholas Kingzett <NSNick@gmail.com>
2016-10-31 14:20:08 -0400

    Added graphics, draw functions

--- chess.py
+++ chess.py
@@ -1,5 +1,6 @@
-import pygame, sys, constants
+import pygame, sys, os, constants
 from pygame.locals import *
+
 
 def main():
     # pygame init and declarations
@@ -9,6 +10,8 @@ def main():
     FPS_CLOCK = pygame.time.Clock()
     # game variables
     black_pieces, white_pieces = generatePieces()
+    current_player = 'white'
+    selected_piece = None
     # game loop
     while True:
         # event handling
@@ -16,12 +19,28 @@ def main():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == MOUSEBUTTONUP:
+                click_coords = (event.pos[0]/constants.TILE_SIZE, event.pos[1]/constants.TILE_SIZE)
+                if event.button == 1 and click_coords[0] < 8:
+                    if selected_piece is not None:
+                        if click_coords in selected_piece:
+                            selected_piece = None
+                    else:
+                        if current_player == 'black' and click_coords in black_pieces.keys():
+                            selected_piece = (click_coords, black_pieces[click_coords])
+                        elif current_player == 'white' and selected_piece in white_pieces.keys():
+                            selected_piece = (click_coords, white_pieces[click_coords])
+                            print 'Piece selected.'
         # drawing to screen
-        # TO DO: if game_mode == INTRO: etc
+        DISPLAY_SURF.fill(constants.BG_COLOR)
         drawBoardTiles(DISPLAY_SURF)
+        drawPieces(DISPLAY_SURF, black_pieces, white_pieces)
+        if selected_piece is not None:
+            drawSelectionSquare(DISPLAY_SURF, selected_piece)
         # display update + clock
         pygame.display.update()
         FPS_CLOCK.tick(constants.FPS)
+
 
 def drawBoardTiles(display):
     for i in range(8):
@@ -32,6 +51,33 @@ def drawBoardTiles(display):
                 tile_color = constants.SQUARE_LIGHT_COLOR
             tile_rect = Rect(i*constants.TILE_SIZE, j*constants.TILE_SIZE, constants.TILE_SIZE, constants.TILE_SIZE)
             pygame.draw.rect(display, tile_color, tile_rect)
+
+
+def loadPieceSprite(color, piece):
+    if (color == 'white' or color == 'black') and piece in ('pawn', 'rook', 'knight', 'bishop', 'queen', 'king'):
+        sprite = pygame.image.load(os.path.join('gfx', color+'_'+piece+'.png')).convert()
+        sprite.set_colorkey(constants.ALPHA_COLOR)
+    else:
+        print str(color) + ', ' + str(piece)
+        raise NameError('loadPieceSprite: invalid color or piece name given.')
+    return sprite
+
+
+def drawPieces(display, black, white):
+    for coords, piece in black.iteritems():
+        piece_image = loadPieceSprite('black', piece)
+        piece_rect = Rect(coords[0]*constants.TILE_SIZE, coords[1]*constants.TILE_SIZE, constants.TILE_SIZE, constants.TILE_SIZE)
+        display.blit(piece_image, piece_rect)
+    for coords, piece in white.iteritems():
+        piece_image = loadPieceSprite('white', piece)
+        piece_rect = Rect(coords[0]*constants.TILE_SIZE, coords[1]*constants.TILE_SIZE, constants.TILE_SIZE, constants.TILE_SIZE)
+        display.blit(piece_image, piece_rect)
+
+
+def drawSelectionSquare(display, selected):
+    selection_rect = (selected[0][0]*constants.TILE_SIZE, selected[0][1]*constants.TILE_SIZE, constants.TILE_SIZE, constants.TILE_SIZE)
+    pygame.draw.rect(display, constants.SELECTION_COLOR, selection_rect, constants.SELECTION_SQUARE_WIDTH)
+
 
 def generatePieces():
     black = {}
@@ -56,6 +102,16 @@ def generatePieces():
     white[(3, 7)] = 'queen'
     white[(4, 7)] = 'king'
     return black, white
+
+
+def newCurrentPlayer(oldPlayer):
+    if oldPlayer == 'white':
+        player = 'black'
+    elif oldPlayer == 'black':
+        player = 'white'
+    else:
+        raise NameError('newCurrentPlayer(): invalid player given.')
+    return player
 
 if __name__ == '__main__':
     main()
